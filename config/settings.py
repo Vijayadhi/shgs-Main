70582d35a12f3f20481f7616097db17b210125a5
Vijayadhi <venerablevignesh@gmail.com>
2024-10-15 19:15:46 +0530

    requiremets commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure--wry&6omnh(-zgbun0dpb$ju=y7e*^)@-d-_0ot*uc(93!+i3h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+]
 
 
 # Application definition
